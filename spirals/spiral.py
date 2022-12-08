6a445ed4e7b6983ddedd0fe71d4eba93cd616409
Felix <sze.chan1996@gmail.com>
2022-12-08 15:37:27 +1100

    Update spiral.py

--- spirals/spiral.py
+++ spirals/spiral.py
@@ -55,4 +55,3 @@ def graph_hidden(net, layer, node):
         # plot function computed by model
         plt.clf()
         plt.pcolormesh(xrange,yrange,pred.cpu().view(yrange.size()[0],xrange.size()[0]), cmap='Wistia') 
-#spiralprocessing 
